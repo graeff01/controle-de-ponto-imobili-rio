cb9695519ba850eb1c60ceb31f87326d715df56f
graeff01 <teu-email-do-github@example.com>
2026-01-11 00:23:37 -0300

    Fix: Trocar bcryptjs por bcrypt

--- backend/src/modules/users/users.service.js
+++ backend/src/modules/users/users.service.js
@@ -1,4 +1,4 @@
-﻿const bcrypt = require('bcryptjs');
+﻿const bcrypt = require('bcrypt');
 const db = require('../../config/database');
 const logger = require('../../utils/logger');
 const auditService = require('../../services/auditService');
